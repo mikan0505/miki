9778a4c5bbfdb612cfeb6f4a651ddcae60120fe8
zm17462 <zm17462@josai.ac.jp>
2018-08-06 03:55:23 -0400

    date

---
+++ myjs_1.js
@@ -0,0 +1,6 @@
+function dodonpa() {
+  document.getElementById('myImage').src='./image/dodonpa.jpg';
+}
+function takabisha() {
+  document.getElementById('myImage').src='./image/takabisha.jpg';
+}
